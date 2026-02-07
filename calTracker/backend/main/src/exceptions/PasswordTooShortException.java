03542dbdd003c30424d8c7c35797d1af51ba0726
nikoi <nikolaspagsisihan0@gmail.com>
2026-02-07 09:54:14 -0500

    create user and meal class

---
+++ calTracker/backend/main/src/exceptions/PasswordTooShortException.java
@@ -0,0 +1,5 @@
+public class PasswordTooShortException extends Exception{
+    public PasswordTooShortException(String message){
+        super(message);
+    }
+}
---
+++ calTracker/backend/main/src/java/Meal.java
@@ -0,0 +1,16 @@
+
+import java.util.Date; //imports the time
+
+public class Meal {
+    //Instance Variables
+    private int id;
+    private String username;
+    private String mealName;
+    private double calories;
+    private double protein;
+    private double carbohydrates;
+    private double fibers;
+    private double fat;
+    private double sugar;
+    private Date timeStamp;
+}
