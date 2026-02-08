b1f9f00eb7b5270d1470e67e6d818e7f05b3d73b
Caleb Dominic Zonaga Gutierrez <czg30930@uga.edu>
2026-02-08 05:57:22 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
 const CACHE_NAME = 'tracalify-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './pages/homePage.html',
+  '/',                // The root
+  '/index.html',      // Explicit index
+  '/pages/homePage.html', 
+  '/manifest.json',   // Highly recommended to cache this!
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;900&display=swap'
 ];
 
-// Install Service Worker and cache assets
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// Fetching assets from cache when offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
